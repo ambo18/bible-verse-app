5d806658d1ef36acc335ac4403310b71f7150f2e
R-jhel B. Tandugon <jherietandugon@gmail.com>
2025-01-06 16:19:25 +0800

    Add a model for storing bible verse

--- backend/bible_verses/models.py
+++ backend/bible_verses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class BibleVerse(models.Model):
+    book = models.CharField(max_length=100)
+    chapter = models.IntegerField()
+    verse = models.IntegerField()
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.book} {self.chapter}:{self.verse}"
