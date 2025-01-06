11a531e92a0f24ed26173e815c8bebbd3fa74f9b
R-jhel B. Tandugon <jherietandugon@gmail.com>
2025-01-06 16:27:55 +0800

    Set up URL routing

--- backend/bible_verses/views.py
+++ backend/bible_verses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import BibleVerse
+from .serializers import BibleVerseSerializer
 
-# Create your views here.
+class BibleVerseList(generics.ListAPIView):
+    queryset = BibleVerse.objects.all()
+    serializer_class = BibleVerseSerializer
+
