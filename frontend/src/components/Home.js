dd83ebc7f3abc33a524689030097124568216bb4
R-jhel B. Tandugon <jherietandugon@gmail.com>
2025-01-09 15:28:49 +0800

    Styled with Tailwind CSS

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Welcome to the Home Page
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
