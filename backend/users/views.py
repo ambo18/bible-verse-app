ffe0fc7ff46987c69ad364a3741a54d36a512515
R-jhel B. Tandugon <jherietandugon@gmail.com>
2025-01-09 15:05:09 +0800

    Add the JWT login endpoint

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-# Create your views here.
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims here
+        token['is_admin'] = user.is_staff
+        return token
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
